aae915f537264d4d55eb52d1dfb77d796fce9bf4
Mohamed Ibrahem <mo7mad369@gmail.com>
2021-03-28 09:48:02 +0200

    last comment

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php	require_once "incloude/database.php";?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Database OMC</title>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database OMC</title>
 
-	<!-- bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet" />
 
 
-	<link rel="stylesheet" href="css/style.css" />
-</head>
-	<body>
-		<?php 
-			include "incloude/document/header.php";
-			include "incloude/addDataaa.php";
-		?>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <?php 
+      include "incloude/document/header.php";
+      include "incloude/addDataaa.php";
+    ?>
 
-		<form method="post"><h2>بحث بالتاريخ</h2><input type ="date" name="filter"/><input type="submit"/></form>
+    <form method="post"><h2>بحث بالتاريخ</h2><input type ="date" name="filter"/><input value="بحث" type="submit"/></form>
 
-		<table class="table">
-			<form method="post" >
-				<?php include "incloude/document/table.php";?>
-			</form>
-		</table>
+    <table class="table">
+      <form method="post">
+        <?php include "incloude/document/table.php";?>
+      </form>
+    </table>
 
-		<script src="js/app.js"></script>
-	</body>
+    <script src="js/app.js"></script>
+  </body>
 </html>
